f225aa873ffa6cdfb0faee968b51251271fe03fa
gonzamontiel <gon.montiel@gmail.com>
2016-11-14 22:35:37 -0300

    make post to server work for all collections

--- postToServer.js
+++ postToServer.js
@@ -1,20 +1,48 @@
 var config = require('config');
 var request = require('request');
 var jsonfile = require('jsonfile');
-var input = 'collections/coursesCol.json';
+var fs = require('fs');
+var Logger = require("./Logger.js");
 
-console.log(config.get('server.host'), "host");
-console.log(config.get('server.port'), "port");
-console.log(config.get('server.api'), "api");
+const FILES_PATH = __dirname + "/scrapping/out/";
 
-request.post(
-    config.server.host,
-    jsonfile.readFileSync(input),
-    function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body);
-        } else {
-            console.log(error, "error");
+var excludes = ".old";
+var inputs = {
+    "events": "Calendar.json",
+    "courses": "Course_courses.json",
+    "departments": "Course_departments.json",
+    "exams": /ExamDates.*\.json/,
+    "news": "News.json"
+};
+
+/*
+* We should be making POST to urls such us:
+* server/api/load/courses
+*
+*/
+for (var collecionName in inputs) {
+    var fileRegexp = inputs[collecionName];
+    fs.readdirSync(FILES_PATH).forEach(function(file) {
+        if (file.match(fileRegexp) !== null && !file.match(excludes)) {
+            var data = jsonfile.readFileSync(FILES_PATH + file);
+            console.log("Posting " + file);
+            request(
+                {
+                    url: "http://localhost:3030/api/load/" + collecionName,
+                    method: 'POST',
+                    json: true,
+                    body: data
+                },
+                ressponse_callback
+            );
         }
+    });
+}
+
+function ressponse_callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        Logger.info(body);
+    } else {
+        console.log(error);
     }
-);
+}
