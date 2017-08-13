84de59526894d4ec27947845097bb2c18afab5d6
gonzamontiel <gon.montiel@gmail.com>
2017-08-12 23:24:56 -0300

    post scrapper result to main server

--- postToServer.js
+++ postToServer.js
@@ -6,43 +6,45 @@ var Logger = require("./Logger.js");
 
 const FILES_PATH = __dirname + "/scrapping/out/";
 
-var excludes = ".old";
-var inputs = {
-    "events": "Calendar.json",
-    "exams": /ExamDates.*\.json/,
-    "news": "News.json"
-    // "courses": "Course_courses.json",
-    // "departments": "Course_departments.json",
-};
-
-/*
-* We should be making POST to urls such us:
-* server/api/load/courses
-*
-*/
-for (var collecionName in inputs) {
-    var fileRegexp = inputs[collecionName];
-    fs.readdirSync(FILES_PATH).forEach(function(file) {
-        if (file.match(fileRegexp) !== null && !file.match(excludes)) {
-            var data = jsonfile.readFileSync(FILES_PATH + file);
-            console.log("Posting " + file);
-            request(
-                {
-                    url: "http://localhost:3030/api/load/" + collecionName,
-                    method: 'POST',
-                    json: true,
-                    body: data
-                },
-                ressponse_callback
-            );
-        }
-    });
-}
-
 function ressponse_callback(error, response, body) {
     if (!error && response.statusCode == 200) {
         Logger.info(body);
     } else {
         console.log(error);
+    }
+}
+
+module.exports = function() {
+    var excludes = ".old";
+    var inputs = {
+        "events": "Calendar.json",
+        "news": "News.json"
+        // "exams": /ExamDates.*\.json/,
+        // "courses": "Course_courses.json",
+        // "departments": "Course_departments.json",
+    };
+
+    /*
+    * We should be making POST to urls such us:
+    * server/api/load/courses
+    *
+    */
+    for (var collecionName in inputs) {
+        var fileRegexp = inputs[collecionName];
+        fs.readdirSync(FILES_PATH).forEach(function(file) {
+            if (file.match(fileRegexp) !== null && !file.match(excludes)) {
+                var data = jsonfile.readFileSync(FILES_PATH + file);
+                console.log("Posting " + file);
+                request(
+                    {
+                        url: "http://localhost:3030/api/load/" + collecionName,
+                        method: 'POST',
+                        json: true,
+                        body: data
+                    },
+                    ressponse_callback
+                );
+            }
+        });
     }
 }
--- run.js
+++ run.js
@@ -1,6 +1,9 @@
 var scrappers = require("./scrapping/");
+var postToServer = require("postToServer.js");
 
 for (var i in scrappers) {
     scrapper = new scrappers[i]();
     scrapper.run();
 }
+
+postToServer();
